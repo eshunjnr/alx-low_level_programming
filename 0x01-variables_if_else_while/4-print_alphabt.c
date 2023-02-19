483931b386bb2d81c0ba10f6d556e86bec9c0225
eshunjnr <eshunjnr@gmial.com>
2023-02-19 13:28:20 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 when successful
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+		if (alp == 'e' || alp == 'q')
+			continue;
+	}
+	return (0);
+}
+

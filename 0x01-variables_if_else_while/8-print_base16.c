f5f1d200a1f69f29fd771c24e6be2317618142fe
eshunjnr <eshunjnr@gmial.com>
2023-02-19 14:59:00 -0800

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (when successful)
+ */
+int main(void)
+{
+	char alp;
+	int i;
+
+	for (i = 0; i <= 16; i++)
+	for (alp = 'a'; alp <= 'f'; alp++)
+	{
+		if (i >= 10)
+			putchar(alp);
+		else if (i < 10)
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

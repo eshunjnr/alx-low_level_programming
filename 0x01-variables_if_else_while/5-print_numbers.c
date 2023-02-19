b684e52123e7356cc4e835718dd2a81038ab617a
eshunjnr <eshunjnr@gmial.com>
2023-02-19 13:59:10 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 11;)
+		printf("%d\n", n);
+	return (0);
+}
